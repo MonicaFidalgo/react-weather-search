12922ef7b52066636059fcdd35e94dece22a1f00
monicafidalgo <fidalgo@evident.nl>
2021-02-07 22:56:23 +0000

    add github link and change html

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import SearchEngine from "./Search";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-content">
         <img src={logo} className="App-logo" alt="logo" />
-        <SearchEngine />
-      </header>
-     
+        <SearchEngine />  
+      </main>
+      <footer> <p>Check my project at <a href="https://github.com/MonicaFidalgo/react-weather-search" className="App-link">Github</a></p></footer>
     </div>
   );
 }
